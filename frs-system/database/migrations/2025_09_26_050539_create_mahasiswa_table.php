74eba2b976f256696cc1932c305659d2d07d1e47
Farrel Jatmiko Aji <176496434+Miko-a@users.noreply.github.com>
2025-09-28 17:41:04 +0700

    add email on mahasiswa

--- frs-system/database/migrations/2025_09_26_050539_create_mahasiswa_table.php
+++ frs-system/database/migrations/2025_09_26_050539_create_mahasiswa_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('mahasiswa', function (Blueprint $table) {
             $table->string('nrp')->primary();
             $table->string('nama');
+            $table->string('email')->unique();
             $table->integer('angkatan');
             $table->string('program_studi');
             $table->integer('semester');
